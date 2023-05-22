ed9bf6d90e3d05efcc701d75e891858d1cc226a9
Ankit BagheL <ak9664773@gmail.com>
2023-05-22 12:26:45 +0530

    recursion increasing no. q's

---
+++ increasingNoRecursion.java
@@ -0,0 +1,15 @@
+public class increasingNoRecursion {
+    public static void increase(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        increase(n-1);
+        System.out.print(n+" ");
+
+    }
+    public static void main(String[] args) {
+        int a=10;
+        increase(a);
+    }
+}
