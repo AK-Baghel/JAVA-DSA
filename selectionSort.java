0ffbfa6f08b194773ae9ad44c5c186ca299db06f
Ankit BagheL <ak9664773@gmail.com>
2023-05-24 17:05:43 +0530

    -

--- selectionSort.java
+++ selectionSort.java
@@ -2,17 +2,19 @@ public class selectionSort {
     public static void main(String[] args) {
         int arr[] = { 4, 2, 1, 5, 6, 3 };
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minPos = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[minPos] > arr[j]) {
-                    minPos = j;
+        
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[minPos];
-            arr[minPos] = temp;
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
         }
+
         for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
