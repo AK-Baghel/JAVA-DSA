78fe8186094d54936fdb38d7d0c2e6bd7aec9b32
Ankit BagheL <ak9664773@gmail.com>
2023-05-31 18:44:10 +0530

    addLast + size of LinkedList

--- LinkedListDSA/LinkedList.java
+++ LinkedListDSA/LinkedList.java
@@ -11,6 +11,7 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size=0;
 
     public static void print(){
         if(head==null){
@@ -22,24 +23,46 @@ public class LinkedList {
             System.out.print(temp.data+"->");
             temp=temp.next;
         }
+        System.out.println();
     }
 
     public static void add(int data){
         Node newNode=new Node(data);
         if(head==null){
-            head=newNode;
+            head=tail=newNode;
+            size++;
             return;
         }
         newNode.next=head;
         head=newNode;
+        size++;
+    }
+
+    public static void last(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            size++;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        size++;
     }
 
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
-        ll.add(1);
-        ll.add(2);
         ll.add(3);
+        ll.add(2);
+        ll.add(1);
+        ll.last(4);
+        ll.last(5);
+        ll.last(6);
         ll.print();
+        System.out.println("Size of LinkedList = "+size);
     }
 }
