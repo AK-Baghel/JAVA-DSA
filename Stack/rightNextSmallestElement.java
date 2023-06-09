a9c58d2f49aebd1696b02f3a3767f092a44d58af
Ankit BagheL <ak9664773@gmail.com>
2023-06-09 12:11:36 +0530

    Next smallest element in left

---
+++ Stack/rightNextSmallestElement.java
@@ -0,0 +1,21 @@
+package Stack;
+import java.util.*;
+public class rightNextSmallestElement {
+    public static void main(String[] args) {
+        int arr[]={6,8,0,1,3};
+        Stack<Integer> s=new Stack<>();
+        int nextSmaller[]=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && arr[i]<arr[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty())
+                nextSmaller[i]=-1;
+            else
+                nextSmaller[i]=arr[s.peek()];
+            s.push(i);
+        }
+        for(int i:nextSmaller)
+            System.out.print(i+" ");
+    }
+}
