0c31d01e86f88ca37e9bea7dfb407bef1c0f02d1
Ankit BagheL <ak9664773@gmail.com>
2023-06-18 13:17:42 +0530

    Preorder Traversal

--- BinaryTrees/bulidTree.java
+++ BinaryTrees/bulidTree.java
@@ -13,6 +13,7 @@ public class bulidTree {
     }
     public static class BinaryTree{
         static int idx=-1;
+       
         public static Node buildTree(int nodes[]){
             idx++;
             if(nodes[idx]==-1)
@@ -23,11 +24,26 @@ public class bulidTree {
 
             return newNode;
         }
+
+        public static void preorder(Node root){
+        if(root==null){
+            System.out.print(-1+"->");
+            return;
+        }
+            
+        System.out.print(root.data+"->");
+        preorder(root.left);
+        preorder(root.right);
     }
+    }
+
+    
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.buildTree(nodes);
         System.out.println(root.data);
+        System.out.print("Preorder traversal = ");
+        tree.preorder(root);
     }
 }
