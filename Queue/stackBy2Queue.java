052f00fcebdb8259f519811d49af9cab474e7f66
Ankit BagheL <ak9664773@gmail.com>
2023-06-18 10:13:35 +0530

    add binary tree build tree

---
+++ Queue/stackBy2Queue.java
@@ -0,0 +1,16 @@
+package Queue;
+import java.util.*;
+public class stackBy2Queue {
+    public static class Stack{
+        static Queue<Integer> q1=new LinkedList<>();
+        static Queue<Integer> q2=new LinkedList<>();
+        
+        
+    }
+    public static void main(String[] args){
+        Stack s=new Stack();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+    }
+}
