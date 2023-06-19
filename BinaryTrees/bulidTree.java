79b030013bd156c4b42ae1d365eff688af9708b0
Ankit BagheL <ak9664773@gmail.com>
2023-06-19 12:06:24 +0530

    LevelOrder Traversal

--- BinaryTrees/bulidTree.java
+++ BinaryTrees/bulidTree.java
@@ -1,5 +1,5 @@
 package BinaryTrees;
-
+import java.util.*;
 public class bulidTree {
     public static class Node {
         int data;
@@ -62,6 +62,32 @@ public class bulidTree {
             System.out.print(root.data + "->");
         }
 
+        public static void levelOrder(Node root){
+            if(root==null)
+                return;
+
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty())
+                        break;
+                    else    
+                        q.add(null);
+                }
+                else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null)
+                        q.add(currNode.left);
+                    if(currNode.right!=null)
+                        q.add(currNode.right);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -81,5 +107,8 @@ public class bulidTree {
         System.out.println();
         System.out.print("Postorder traversal = ");
         tree.postorder(root);
+
+        System.out.println();
+        tree.levelOrder(root);
     }
 }
