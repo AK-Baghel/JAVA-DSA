b1edbfbe4ba3bcff61ac781ac96192dee45b1314
Ankit BagheL <ak9664773@gmail.com>
2023-08-11 12:11:47 +0530

    Sorted Array to B.S.T.

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -159,6 +159,29 @@ public class BST {
 
         return root;
     }
+
+
+    //Preorder B.S.T.
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    //Sorted Array To Balanced B.S.T.
+    public static Node createBST(int arr[],int s,int e){
+        if(s>e){
+            return null;
+        }
+        int mid=(s+e)/2;
+        Node root=new Node(arr[mid]);
+        root.left=createBST(arr, s, mid-1);
+        root.right=createBST(arr, mid+1,e);
+        return root;
+    }
     
     
     public static void main(String[] args) {
@@ -204,8 +227,14 @@ public class BST {
         inorder(root);
         System.out.println();
 
-        
+        //Sorted Array To Balanced B.S.T.
+        int arr[]={3,5,6,8,10,11,12};
+        Node newNode=createBST(arr,0,arr.length-1);
+        System.out.print("Sorted Array to B.S.T. = ");
+        preorder(newNode);
         System.out.println();
+        System.out.println();
+
 
     }
 }
